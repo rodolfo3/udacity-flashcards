b07b0af4e1a43c2e7a07de16204517cd6b8ea5fb
rodolfo3 <rodolfo.3@gmail.com>
2018-01-11 13:22:32 -0200

    [wip] CardList component

---
+++ components/CardList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+class CardList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CardList</Text>
+      </View>
+    );
+  }
+}
+
+
+export default CardList;
