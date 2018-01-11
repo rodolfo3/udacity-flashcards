a675ed151389c7fa3ccc7f795958b38e09d28362
rodolfo3 <rodolfo.3@gmail.com>
2018-01-11 13:33:38 -0200

    status-bar

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { Constants } from 'expo';
+
 import CardList from './components/CardList';
+
+
+const AppStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
 
 export default class App extends React.Component {
   render() {
-    return <CardList />;
+    return (
+      <View style={{flex: 1}}>
+        <AppStatusBar backgroundColor="white" />
+        <CardList />
+      </View>
+    );
   }
 }
