7704bd99656677179640788be16fece848494ca7
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2018-01-14 19:27:23 -0200

    wip: add more mock data (to scroll) + return promises

--- utils/db.js
+++ utils/db.js
@@ -15,13 +15,61 @@ const mock = {
         answer: 'The componentDidMount lifecycle event',
       },
     ],
-  }
+  },
+  'id43': {
+    id: 'id43',
+    title: 'React',
+    questions: [
+      {
+        id: 'id314',
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces',
+      },
+      {
+        id: 'id1592',
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event',
+      },
+    ],
+  },
+  'id44': {
+    id: 'id44',
+    title: 'React',
+    questions: [
+      {
+        id: 'id314',
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces',
+      },
+      {
+        id: 'id1592',
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event',
+      },
+    ],
+  },
+  'id45': {
+    id: 'id45',
+    title: 'React',
+    questions: [
+      {
+        id: 'id314',
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces',
+      },
+      {
+        id: 'id1592',
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event',
+      },
+    ],
+  },
 };
 
 
 export function getDecks() {
   return new Promise(function(resolve) {
-    setTimeout(() => resolve(mock), 1500);
+    setTimeout(() => resolve(mock), 150);
   })
 }
 
@@ -37,14 +85,19 @@ export function getDeck(id) {
 }
 
 
+function generateId() {
+  return String(parseInt(Math.random() * 10e15));
+}
+
+
 export function saveDeck({ title }) {
   const deck = {
-    id: String(Math.random()),
+    id: generateId(),
     title,
     questions: []
   }
   mock[deck.id] = deck;
-  return deck;
+  return Promise.resolve(deck);
 }
 
 
